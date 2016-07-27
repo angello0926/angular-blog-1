2c85b734a5aac566a05d97cfdbfae0cbbed238d9
Denis Cheung <d.s.h.cheung@gmail.com>
2016-07-27 16:36:53 +0800

    added  crud

--- app/assets/javascripts/controllers/modal_instance_controller.js
+++ app/assets/javascripts/controllers/modal_instance_controller.js
@@ -1,9 +1,30 @@
-app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 'data', function($scope, $uibModalInstance, data){
+app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 'data', '$http', function($scope, $uibModalInstance, data, $http){
+  var success = function (resp) {
+    console.log(resp);
+    $uibModalInstance.close(resp.data);
+  };
+
+  var error = function (resp) {
+    console.log(resp);
+  };
+
   $scope.mode = data.mode;
   $scope.post = angular.copy(data.post);
 
-  $scope.create = function () {
-    $uibModalInstance.close($scope.post);
+  $scope.createOrUpdate = function () {
+    if ($scope.mode == "create") {
+      $http({
+        url: 'http://localhost:3000/posts.json',
+        method: 'POST',
+        data: $scope.post
+      }).then(success, error);
+    } else if ($scope.mode == "edit") {
+      $http({
+        url: 'http://localhost:3000/posts/' + $scope.post.id + '.json',
+        method: 'PUT',
+        data: $scope.post
+      }).then(success, error);
+    }
   };
 
   $scope.cancel = function () {
